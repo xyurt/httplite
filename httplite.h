3eb6db8d7cf0c7b3be733fe2c1dc9758f2629e7e
xyurt <yurtrimu@gmail.com>
2025-07-14 19:18:39 +0300

    Update httplite.h
    Changed function return values to 'char' for using less memory.
    Removed 'size_t' in order to receive a project that uses no dependencies
    
    Fixed the algorithm so it accepts empty header values which is valid.
    Made the code compactness less error-prone.

--- httplite.h
+++ httplite.h
@@ -2,23 +2,23 @@
 #define HTTP_PARSER 
 typedef struct http_message {
     const char *part1; /* Method field for request, Version field for response */
-    size_t part1_length;
+    int part1_length;
     const char *part2; /* Path field for request, Code field for response */
-    size_t part2_length;
+    int part2_length;
     const char *part3; /* Version field for request, Reason field for response */
-    size_t part3_length;
-    const char *next;  /* Pointer to continue reading the headers and the body as a stream */
-    size_t next_length;
+    int part3_length;
+    char *next;  /* Pointer to continue reading the headers and the body as a stream */
+    int next_length;
 } http_message;
-static int http_next_header(http_message *message, const char **name, size_t *name_length, const char **value, size_t *value_length) {
+static char http_next_header(http_message *message, char **name, int *name_length, char **value, int *value_length) {
     if (message->next_length < 2) return 0;
     if (message->next[0] == '\r' && message->next[1] == '\n') {
         message->next += 2;
         message->next_length -= 2;
-        *name_length = *value_length = *name = *value = 0;
+        *name = *value = 0, *name_length = *value_length = 0;
         return 1;
     }
-    size_t next_offset = 0;
+    int next_offset = 0;
     *name_length = *value_length = 0;
     *name = &message->next[next_offset];
     int colon_found = 0;
@@ -26,13 +26,13 @@ static int http_next_header(http_message *message, const char **name, size_t *na
     if (!colon_found || *name_length == 0) return 0;
     *value = &message->next[next_offset];
     while (next_offset + 1 < message->next_length && message->next[next_offset++] != '\r') (*value_length)++;
-    if (*value_length == 0 || next_offset + 1 >= message->next_length || message->next[next_offset++] != '\n') return 0;
+    if (next_offset + 1 >= message->next_length || message->next[next_offset++] != '\n') return 0;
     message->next = &message->next[next_offset];
     message->next_length -= next_offset;
     return 1;
 }
-static int http_parse_message(const char *buffer, size_t buffer_size, http_message *out) {
-    size_t buffer_offset = 0;
+static char http_parse_message(char *buffer, int buffer_size, http_message *out) {
+    int buffer_offset = 0;
     out->part1_length = out->part2_length = out->part3_length = out->next_length = 0;
     out->part1 = &buffer[buffer_offset];
     while (buffer_offset + 1 < buffer_size && buffer[buffer_offset++] != ' ') out->part1_length++;
